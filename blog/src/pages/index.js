b29f0a3730c56a6e1f1f85685ffb17b21ef5671d
Jesslynwong <jesslynwjx@gmail.com>
2022-12-05 09:52:17 +0800

    feat: blog website cover

---
+++ blog/src/pages/index.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Hero from '../../static/img/Hero.jpg'
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+const svgList = [
+  {
+    title: 'github',
+    Svg: require('../../static/img/github.svg').default,
+    color: 'black',
+    link: 'https://github.com/Jesslynwong',
+  },
+  // {
+  //   title: 'bilibili',
+  //   Svg: require('../../static/img/bilibili.svg').default,
+  //   link: 'https://space.bilibili.com/223211771',
+  // },
+  {
+    title: 'csdn',
+    Svg: require('../../static/img/csdn.svg').default,
+    color: '#2979ff',
+    link: 'https://blog.csdn.net/m0_50233720?type=blog',
+  },
+]
+const Svg = ({ Svg, color, title, link }) => {
+  return (
+    <a href={link} target='_blank'>
+      <Svg className={styles.svg} style={{ fill: color }} />
+    </a>
+  )
+}
+
+function Introduction() {
+  return (
+    <div className={styles.myHeroContainer}>
+      <div className={styles.leftContainer}>
+        <h1 className={styles.leftContainer_h1}>
+          Hello<br/>Welcome to <br/>Jesslyn's World
+        </h1>
+        <p className={styles.leftContainer_p}>
+          一个热爱探索，好奇心重的程序媛👧
+          <br />
+          在这里记录知识，希望对你也有帮助 🏖️
+        </p>
+        <div className={styles.buttonContainer}>
+          <div className={styles.svgContainer}>
+            {svgList.map((item, index) => {
+              return <Svg {...item} key={item.title} />
+            })}
+          </div>
+        </div>
+      </div>
+      <div className={styles.rightContainer}>
+        <img src={Hero} alt='HeroImg' />
+      </div>
+    </div>
+  )
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      {/* <HomepageHeader /> */}
+      {/* <main> */}
+        {/* <HomepageFeatures /> */}
+        <Introduction/>
+      {/* </main> */}
+    </Layout>
+  );
+}
